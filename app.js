b25eebca45e7395cb78f6188e91657b1c451b334
Gilang Arya Pradana <gilangarya272001@gmail.com>
2025-09-28 01:20:43 +0700

    add login and signup routes with form parsing middleware

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
 });
 
 module.exports = app;
