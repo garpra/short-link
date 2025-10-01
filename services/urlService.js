6a91d3326ce796d00ed323ced305bf1b6cc862af
Gilang Arya Pradana <gilangarya272001@gmail.com>
2025-10-01 22:07:17 +0700

    add URL service for CRUD operations and click tracking

---
+++ services/urlService.js
@@ -0,0 +1,41 @@
+const db = require("../database");
+
+function getUrlsByUserId(userId) {
+  return db
+    .prepare(`SELECT * FROM urls WHERE user_id = @id`)
+    .all({ id: userId });
+}
+
+function createUrl(userId, shortCode, originalUrl) {
+  return db
+    .prepare(
+      `INSERT INTO urls (user_id, short_code, original_url, created_at) VALUES (?,?,?,?)`,
+    )
+    .run(userId, shortCode, originalUrl, Date.now());
+}
+
+function deleteUrl(urlId) {
+  return db.prepare("DELETE FROM urls WHERE id = ?").run(urlId);
+}
+
+function getUrlByShortCode(shortCode) {
+  return db
+    .prepare("SELECT * FROM urls WHERE short_code = @shortCode")
+    .get({ shortCode });
+}
+
+function incrementClickCount(shortCode) {
+  return db
+    .prepare(
+      "UPDATE urls SET click_count = click_count + 1 WHERE short_code = @shortCode",
+    )
+    .run({ shortCode });
+}
+
+module.exports = {
+  getUrlsByUserId,
+  createUrl,
+  deleteUrl,
+  getUrlByShortCode,
+  incrementClickCount,
+};
