eb70a1924b6d6c67579a7f08443aa8af970ac479
Gilang Arya Pradana <gilangarya272001@gmail.com>
2025-10-02 00:35:19 +0700

    add delete modal and render user URLs with table actions

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -16,6 +16,64 @@ document.addEventListener("DOMContentLoaded", async () => {
             `;
     tableContainer.hidden = !tableContainer.hidden;
 
+    const fetchUrls = await fetch("/user/urls");
+    const urls = await fetchUrls.json();
+
+    const tableBody = document.getElementById("urls-table-body");
+    tableBody.innerHTML = "";
+
+    urls.forEach((url) => {
+      const date = new Date(url.created_at);
+
+      const tableRow = `
+                <tr>
+                  <td class="px-4 py-3 border-secondary">
+                    <a
+                      href="/${url.short_code}"
+                      target="_blank"
+                      class="text-decoration-none text-primary fw-medium d-block mb-1"
+                    >
+                      <i class="bi bi-link-45deg"></i> sho.rt/${url.short_code}
+                    </a>
+                    <span class="text-secondary" style="font-size: 0.8rem">
+                      Added ${date.toLocaleDateString()}
+                    </span>
+                  </td>
+
+                  <td class="py-3 border-secondary text-truncate" style="max-width: 250px">
+                    <span class="text-light">${url.original_url}</span>
+                  </td>
+
+                  <td class="text-center py-3 border-secondary">
+                    <span class="badge bg-dark border border-secondary text-light">
+                      ${url.click_count}
+                    </span>
+                  </td>
+
+                  <td class="text-end px-4 py-3 border-secondary">
+                    <button
+                      class="btn btn-sm btn-outline-secondary rounded-circle me-1"
+                      title="Copy"
+                    >
+                      <i class="bi bi-clipboard"></i>
+                    </button>
+
+                    <button
+                      class="btn btn-sm btn-outline-secondary rounded-circle me-1 delete-btn"
+                      title="Delete"
+                      data-id="${url.id}"
+                      data-bs-toggle="modal"
+                      data-bs-target="#deleteModal"
+                    >
+                      <i class="bi bi-trash"></i>
+                    </button>
+                  </td>
+                </tr>
+                `;
+
+      tableBody.insertAdjacentHTML("beforeend", tableRow);
+    });
+
     const logoutBtn = document.getElementById("logout-button");
     logoutBtn.addEventListener("click", async () => {
       await fetch("/auth/logout");
