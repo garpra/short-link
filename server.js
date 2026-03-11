01377a10bea32bac1ee6679fa85a7bb405760a22
Gilang Arya Pradana <gilangarya272001@gmail.com>
2026-03-11 20:29:44 +0700

    feat: add simple route express

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const db = require("./database");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
+});
+
+app.listen(port);
